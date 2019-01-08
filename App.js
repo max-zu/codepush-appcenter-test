b8dd165c29dc8288e4c169c68f47de7794a9b361
max-zu <zubovmaksim@gmail.com>
2019-01-08 17:56:49 +0300

    test env vars

--- App.js
+++ App.js
@@ -39,6 +39,7 @@ class App extends Component<Props> {
         <Text style={styles.welcome}>Welcome to React Native!!!!!!!!</Text>
         <Text style={styles.instructions}>To get started, edit App.js!!!!!!</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.instructions}>{process.env.TEST_VAR}</Text>
         <TouchableOpacity onPress={this.onButtonPress}>
           <Text>Check for updates</Text>
         </TouchableOpacity>
